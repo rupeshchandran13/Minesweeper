a08c1aa246e3bb9eb90aec593159066e4d840e11
rupesh <rupeshchandran13@gmail.com>
2023-07-17 12:07:47 -0400

    Added main, settings and utils files

---
+++ main.py
@@ -0,0 +1,39 @@
+from tkinter import *
+import settings
+import utils
+
+root = Tk()
+# Override the settings of the window
+root.configure(bg='black')
+root.geometry(f'{settings.WIDTH}x{settings.HEIGHT}')
+root.title("Minesweeper Game")
+root.resizable(False, False)
+
+top_frame = Frame(
+    root,
+    bg='black',  # change back to black
+    width=settings.WIDTH,
+    height=utils.height_prct(25),
+)
+top_frame.place(x=0, y=0)
+
+left_frame = Frame(
+    root,
+    bg='black',  # change back to black
+    width=utils.width_prct(25),
+    height=utils.height_prct(75)
+)
+left_frame.place(x=0, y=utils.height_prct(25))
+
+centre_frame = Frame(
+    root,
+    bg='black',  # change back to black
+    width=utils.width_prct(75),
+    height=utils.height_prct(75)
+)
+centre_frame.place(
+    x=utils.width_prct(25),
+    y=utils.height_prct(25)
+)
+
+root.mainloop()
